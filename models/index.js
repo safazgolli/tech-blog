75a0d25bfd92471f6c685ed514dcc2c39bb0110e
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-10-25 23:49:18 -0400

    first commit

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./coments');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
+
+
